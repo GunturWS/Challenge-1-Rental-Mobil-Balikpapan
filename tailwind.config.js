80148b714de93413a031e799b04cff58e0dfc84e
gunturrr <gunturwisnu72@gmail.com>
2023-08-25 22:05:32 +0800

    update : updatenew

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,18 +4,15 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: "Helvetica,Arial, sans-serif",
+        sans: "Helvetica",
       },
       colors: {
         Biru: "#0D28A6",
         Hijau: "#5CB85F",
         "Hijau Lemon": "#92D094",
-        Kuning: "#F9CC00",
-        Merah: "#FA2C5A",
         "Hijau muda": "#5CB85F",
         "abu-abu": "#CFD4ED",
         "semi-putih": "#F1F3FF",
-        "Biru-tua": "#0D28A6",
         border: "#D0D0D0",
       },
     },
